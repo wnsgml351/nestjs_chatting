f03702ecc2f4427aefed7262b133b4d1a4950d28
LEE JUN HEE <wnsgml351@naver.com>
2024-09-18 23:08:34 +0900

    nestjs 랜덤채팅웹앱 셋업 추가

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {}),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule implements NestModule {
+  configure() {
+    const DEBUG = process.env.MODE === 'dev' ? true : false;
+    mongoose.set('debug', DEBUG);
+  }
+}
